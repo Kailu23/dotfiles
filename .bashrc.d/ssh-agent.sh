2c42fcb37e1782af154ae7fe3434b3769cac9d3b
Marin Valenčak <valencakmarin@gmail.com>
2026-02-11 19:45:38 +0100

    Update ssh-agent.sh

--- .bashrc.d/ssh-agent.sh
+++ .bashrc.d/ssh-agent.sh
@@ -1,11 +1,23 @@
-if [ -z "$SSH_AUTH_SOCK" ]; then
-	if [ -f "$HOME/.ssh/agent-info" ]; then
-		source "$HOME/.ssh/agent-info"
-	else
-		eval "$(ssh-agent -s)"
-		echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK" > "$HOME/.ssh/agent-info"
-		echo "export SSH_AGENT_PID=$SSH_AGENT_PID" >> "$HOME/.ssh/agent-info"
-	fi
+[[ $- != *i* ]] && return
+
+SSH_ENV="$HOME/.ssh/agent-info"
+
+start_agent(){
+	eval "$(ssh-agent -s)" >/dev/null
+	{
+		echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK"
+		echo "export SSH_AGENT_PID=$SSH_AGENT_PID"
+	} > "$SSH_ENV"
+	chmod 600 "$SSH_ENV"
+}
+
+if [ -f "$SSH_ENV" ]; then
+	source "$SSH_ENV" >/dev/null
 fi
 
-ssh-add -q ~/Documents/Marin/GitHub/.ssh/id_rsa 2>/dev/null
+if ! ssh-add -l >/dev/null 2>&1; then
+	start_agent
+fi
+
+ssh-add -l | grep -q id_rsa || \
+ssh-add ~/Documents/Marin/GitHub/.ssh/id_rsa 
