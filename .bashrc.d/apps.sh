7460e30382196f0a6675f45e4ef6a364f7127f18
Marin Valenčak <valencakmarin@gmail.com>
2026-03-19 12:55:51 +0100

    Add alias for Neovim

--- .bashrc.d/apps.sh
+++ .bashrc.d/apps.sh
@@ -4,3 +4,5 @@ vsCodePath="C:/Users/Marin/AppData/Local/Programs/Microsoft VS Code/Code.exe"
 alias studio='cmd //c start "" "$studioPath"'
 
 alias code='cmd //c start "" "$vsCodePath"'
+
+alias n='nvim'
